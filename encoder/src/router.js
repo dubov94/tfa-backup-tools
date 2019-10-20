3d746cc02e0685ffdaa899d993528961e82331f8
Pavel Dubov <dubov94@gmail.com>
2019-10-20 06:31:41 +0100

    Rename Router to VueRouter

--- encoder/src/router.js
+++ encoder/src/router.js
@@ -1,14 +1,12 @@
 import Index from '@/components/Index.vue'
-import Router from 'vue-router'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Index
-    }
-  ]
+export default new VueRouter({
+  routes: [{
+    path: '/',
+    component: Index
+  }]
 })
