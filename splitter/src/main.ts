0f411aff0352593ec14bc05267741f19adc668ab
Pavel Dubov <dubov94@gmail.com>
2022-04-18 11:05:11 +0100

    Install Vuetify for tfa-backup-splitter

--- splitter/src/main.ts
+++ splitter/src/main.ts
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Application from '@/Application.vue'
 import router from '@/router'
 import store from '@/store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(Application)
 }).$mount('#application')
