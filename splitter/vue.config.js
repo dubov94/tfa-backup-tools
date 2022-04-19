3a7128ef0199b4f1ca33b684d80e4cfae095421a
Pavel Dubov <dubov94@gmail.com>
2022-04-19 23:35:18 +0100

    Inject CSP with production constraints

--- splitter/vue.config.js
+++ splitter/vue.config.js
@@ -10,5 +10,33 @@ module.exports = defineConfig({
         crypto: false
       }
     }
+  },
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        const constructTemplateParameters = args[0].templateParameters
+        args[0].templateParameters = (...args) =>{
+          const parameters = constructTemplateParameters(...args)
+          parameters.CSP = ''
+          if (process.env.NODE_ENV === 'production') {
+            parameters.CSP = [
+              "default-src 'none'",
+              "script-src 'self'",
+              [
+                'style-src',
+                "'self'",
+                // `vuetify.Theme.genStyleElement`
+                "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+                // `vuetify.Theme.css`
+                "'sha256-H0hALk5rjM4aGisbF9hx4t4yVUsKf0ZX1N28nouwAhI='"
+              ].join(' '),
+              "font-src 'self' data:"
+            ].join('; ')
+          }
+          return parameters
+        }
+        return args
+      })
   }
 })
