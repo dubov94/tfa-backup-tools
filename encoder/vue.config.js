91e453520764aeac91896c6c73dcc70f75758858
Pavel Dubov <dubov94@gmail.com>
2022-04-20 02:36:34 +0100

    Allow PDF in `<iframe>` by CSP

--- encoder/vue.config.js
+++ encoder/vue.config.js
@@ -21,12 +21,16 @@ module.exports = {
                 // `vuetify.Theme.genStyleElement`
                 "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
                 // `vuetify.Theme.css`
-                "'sha256-H0hALk5rjM4aGisbF9hx4t4yVUsKf0ZX1N28nouwAhI='"
+                "'sha256-H0hALk5rjM4aGisbF9hx4t4yVUsKf0ZX1N28nouwAhI='",
+                // https://github.com/parallax/jsPDF/blob/2d9a91916471f1fbe465dbcdc05db0cf22d720ec/src/jspdf.js#L3169
+                "'sha256-cQ06adU3/uMUMHtBZsElkcA8pCtSaVI8ThTIFgr+348='"
               ].join(' '),
               // https://security.stackexchange.com/a/95011
               "font-src 'self' data:",
-              // https://security.stackexchange.com/a/95011
-              "img-src 'self' data:"
+              // https://github.com/soldair/node-qrcode#todataurltext-options-cberror-url-1
+              "img-src 'self' data:",
+              // https://github.com/parallax/jsPDF/blob/2d9a91916471f1fbe465dbcdc05db0cf22d720ec/src/jspdf.js#L3086
+              "frame-src data:"
             ].join('; ')
           }
           return parameters
